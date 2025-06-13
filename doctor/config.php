d67362a549100b6acb5da4db6adf1eca81ba2b50
tiako123 <anaellatiako9@gmail.com>
2025-06-13 11:23:25 +0100

    doctor

--- doctor/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-try {
-    $conn = new PDO("mysql:host=localhost;dbname=gestion_sante", "root", "");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    $conn->exec("SET NAMES 'utf8'");
-} catch (PDOException $e) {
-    die("Connexion échouée : " . htmlspecialchars($e->getMessage()));
-}
-?>
